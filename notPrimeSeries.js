cd161748a575f22fed8bc867450cc9620ea2926e
Vipul Kumar <73470976+vipullsingh@users.noreply.github.com>
2022-12-01 23:20:35 +0530

    Create notPrimeSeries.js

---
+++ notPrimeSeries.js
@@ -0,0 +1,18 @@
+function notPrimeSeries(num)
+  {
+    let str = ''
+    for(let i=2;i<=num;i++)
+      {
+        count = 0
+        for(j=2;j<=i;j++)
+          {
+            if(i%j==0)
+              count++
+          }
+        if(count!=1)
+          str=str+i+' '
+      }
+    return(str)
+  }
+let n = 100
+console.log(notPrimeSeries(n))
